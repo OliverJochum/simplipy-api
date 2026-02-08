0ea58641adb273104f17ee0339d80fbb4733badf
Oliver Jochum <oliver.jochum@yahoo.com>
2026-02-09 00:47:26 +0100

    Add interface for scores

---
+++ analysis/score.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+class Score(ABC):
+
+    # Calculate score for candidate text against reference text (generally used for context retention scores, e.g. BERTScore)
+    # Returns a float score value, higher is better
+    @abstractmethod
+    def calculate(cands: list[str], refs: list[str]) -> float:
+        pass
